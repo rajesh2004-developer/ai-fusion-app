f2a56aebb0c1bb7e8b7e805bb7aab4fff7c4a61c
rajesh2004-developer <iamrajesh65892004@gmail.com>
2025-09-20 09:21:15 +0530

    project setup

---
+++ app/Provider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+function Provider({ children, ...props }) {
+  return (
+    <NextThemesProvider
+      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div>{children}</div>
+    </NextThemesProvider>
+  );
+}
+
+export default Provider;
